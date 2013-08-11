1e41498cdcd822f702d0e7c376df80bdcdaa5606
Einar Lielmanis <einars@gmail.com>
2013-08-11 05:15:55 +0300

    Genetic searcher

--- src/guesser.ml
+++ src/guesser.ml
@@ -37,6 +37,8 @@ let build_program desc =
 
     let n_ops = Array.length desc.operators in
     let adj = if context.inside_fold then 5 else 3 in
+    let adj = if adj = 3 && not context.allow_zero then 2 else 3 in
+    let adj = if (adj = 3 || adj = 2) && not context.allow_const then 1 else adj in
     let r = (Random.int (n_ops + adj)) - adj in
     let nptr = ptr + 1 in
 
@@ -62,7 +64,7 @@ let build_program desc =
 
     if op = "if0" then
       let e1, nptr = get_expr {n_context with allow_const = false } nptr in
-      let e2, nptr = get_expr context_skip_zero nptr in
+      let e2, nptr = get_expr n_context nptr in
       let e3, nptr = get_expr n_context nptr in
       If0 (e1, e2, e3), nptr
     else
@@ -101,7 +103,7 @@ let build_program desc =
 
   in
   let expr, expr_size = get_expr default_guess_context 0 in
-  if expr_size == 1 then (
+  if expr_size = 1 then (
     (* Helpers.say "uncool (%d, want %d) %s" expr_size size (Program.expr_to_s expr); *)
     raise Nuff;
   );
@@ -118,7 +120,6 @@ let good_random_guess desc =
 
 
 let suitable_first_inputs () =
-  let base =
     [ 0xffffffffffffffffL
     ; 0x0000000000000000L
     ; 0x8000000000000000L
@@ -128,13 +129,15 @@ let suitable_first_inputs () =
     ; 0x0123456789abcdefL
     ; 0xfedcba9876543210L
     ; 0x01020305070b0d11L
-    ] in
+    ]
 
+  (*
   let rec append_random accum = function
     | 0 -> accum
     | n -> append_random ((Random.int64 0x7ffffffffffffffeL) :: accum) (n - 1)
   in
   append_random base 10
+  *)
 
 
 
@@ -155,135 +158,230 @@ let rot = Helpers.make_rotator ()
 
 exception Abort_this_crap
 
-let rec smart_iterate_problemspace desc verify_fn : unit =
+type pool_entry_t =
+  { pe_dna : int array
+  ; pe_program : program
+  ; pe_score : int
+  }
+
+
+exception Ret_something of program
+exception Candidate_update
+
+let rec smart_iterate_problemspace desc verify_fn input_g : unit =
 
   let n_ops = Array.length desc.operators in
 
-  let ttl = ref 1000 in
 
-  let rec with_matching_exprs ptr context builder_f =
-
-    if ptr > 0 then (
-      ttl := !ttl - 1;
-      if !ttl = 0 then raise Abort_this_crap;
-    );
-
-    let nptr = ptr + 1 in
-    let n_context = { context with allow_not = true; allow_const = true } in (* reset allow_not status *)
-    let context_skip_zero = { n_context with allow_zero = false } in
-
-    let do_expr r =
-      if ptr > desc.problem_size then raise Nuff;
-
-      if r = -1 then if context.allow_const && context.allow_zero then (builder_f E_0 nptr);
-      if r = -2 then if context.allow_const then (builder_f E_1 nptr);
-      if r = -3 then ( builder_f (Identifier "x") nptr );
-      if (r = -4 && context.inside_fold) then ( builder_f (Identifier "y1") nptr );
-      if (r = -5 && context.inside_fold) then ( builder_f (Identifier "y2") nptr );
-
-      if r >= 0 then begin
-        (* let op = desc.operators.(r) in *)
-
-        let op = desc.operators.(r) in
-        if op = "not" && (not context.allow_not) then raise Nuff;
-        if op = "fold" && (not context.allow_fold) then raise Nuff;
-        if op = "fold" && (context.inside_fold) then raise Nuff;
-        if op = "tfold" && (not context.allow_fold) then raise Nuff;
-        if op = "tfold" && (context.inside_fold) then raise Nuff;
-        if op = "bonus" then raise Nuff;
-
-        (* op3 *)
-        if op = "if0" then (
-          with_matching_exprs nptr {n_context with allow_const = false }
-            (fun e1 nptr -> with_matching_exprs nptr context_skip_zero
-                (fun e2 nptr -> with_matching_exprs nptr n_context
-                    (fun e3 nptr -> builder_f (If0 (e1, e2, e3)) nptr )))
-        ) else
-        (* op1 *)
-        if op = "not" then (
-          with_matching_exprs nptr {n_context with allow_not = false}
-            (fun e1 nptr -> builder_f (Not e1) nptr )
-        ) else
-        if op = "shl1" then (
-          with_matching_exprs nptr context_skip_zero
-            (fun e1 nptr -> builder_f (Shl1 e1) nptr )
-        ) else
-        if op = "shr1" then (
-          with_matching_exprs nptr context_skip_zero
-            (fun e1 nptr -> builder_f (Shr1 e1) nptr )
-        ) else
-        if op = "shr4" then (
-          with_matching_exprs nptr context_skip_zero
-            (fun e1 nptr -> builder_f (Shr4 e1) nptr )
-        ) else
-        if op = "shr16" then (
-          with_matching_exprs nptr context_skip_zero
-            (fun e1 nptr -> builder_f (Shr16 e1) nptr )
-        ) else
-        (* op2 *)
-        if op = "and" then (
-          with_matching_exprs nptr context_skip_zero
-            (fun e1 nptr -> with_matching_exprs nptr context_skip_zero
-                (fun e2 nptr -> builder_f (And (e1, e2)) nptr ))
-        ) else
-        if op = "or" then (
-          with_matching_exprs nptr context_skip_zero
-            (fun e1 nptr -> with_matching_exprs nptr context_skip_zero
-                (fun e2 nptr -> builder_f (And (e1, e2)) nptr ))
-        ) else
-        if op = "xor" then (
-          with_matching_exprs nptr context_skip_zero
-            (fun e1 nptr -> with_matching_exprs nptr context_skip_zero
-                (fun e2 nptr -> builder_f (And (e1, e2)) nptr ))
-        ) else
-        if op = "plus" then (
-          with_matching_exprs nptr context_skip_zero
-            (fun e1 nptr -> with_matching_exprs nptr context_skip_zero
-                (fun e2 nptr -> builder_f (And (e1, e2)) nptr ))
-        ) else
-        if op = "tfold" then (
-          with_matching_exprs nptr { n_context with allow_fold = false }
-            (fun e1 nptr -> with_matching_exprs nptr { n_context with allow_fold = false; inside_fold = true }
-                (fun e2 nptr -> builder_f (Fold (e1, E_0, "y1", "y2", e2)) nptr ))
-        ) else
-        if op = "fold" then (
-          with_matching_exprs nptr { n_context with allow_fold = false }
-            (fun e1 nptr -> with_matching_exprs nptr { n_context with allow_fold = false }
-                (fun e2 nptr -> with_matching_exprs nptr { n_context with allow_fold = false; inside_fold = true }
-                    (fun e3 nptr -> builder_f (Fold (e1, e2, "y1", "y2", e3)) nptr )))
-        ) else failwith ("what is " ^ op);
-      end
-
-      in
+  let problem_of_dna dna =
+    let rec with_matching_exprs ptr context builder_f =
 
 
-      (*
-      let shuffled d =
-          let nd = List.map (fun c -> (Random.bits (), c)) d in
-          let sond = List.sort compare nd in
-          List.map snd sond
-      in
+      if ptr >= desc.problem_size then raise Nuff;
 
-      List.iter (fun x -> try do_expr x with Nuff -> ()) (shuffled [-4; -3; -2; -1; 0; 1; 2; 3; 4; 5; 6; 7; 8; 9; 10; 11; 12; 13; 14; 15; 16; 17])
-      *)
-      (*
-      for i = -4 to 15 do
-        try do_expr i with Nuff -> ()
-      done
-      *)
-      for i = 1 to (* n_ops - ptr + *) 3 do
-        try do_expr ((Random.int (n_ops + 5)) - 5) with Nuff -> ();
-      done
+      let nptr = ptr + 1 in
+      let n_context = { context with allow_not = true; allow_const = true } in (* reset allow_not status *)
+      let context_skip_zero = { n_context with allow_zero = false } in
 
+      let do_expr r =
+        if r = -1 then if context.allow_const && context.allow_zero then (builder_f E_0 nptr);
+        if r = -2 then if context.allow_const then (builder_f E_1 nptr);
+        if r = -3 then ( builder_f (Identifier "x") nptr );
+        if (r = -4 && context.inside_fold) then ( builder_f (Identifier "y1") nptr );
+        if (r = -5 && context.inside_fold) then ( builder_f (Identifier "y2") nptr );
+
+        if r >= 0 then begin
+          (* let op = desc.operators.(r) in *)
+
+          let op = desc.operators.(r) in
+          if op = "not" && (not context.allow_not) then raise Nuff;
+          if op = "fold" && (not context.allow_fold) then raise Nuff;
+          if op = "fold" && (context.inside_fold) then raise Nuff;
+          if op = "tfold" && (not context.allow_fold) then raise Nuff;
+          if op = "tfold" && (context.inside_fold) then raise Nuff;
+
+          if op = "bonus" then raise Nuff;
+
+          (* op3 *)
+          if op = "if0" then (
+            with_matching_exprs nptr {n_context with allow_const = false }
+              (fun e1 nptr -> with_matching_exprs nptr n_context
+                  (fun e2 nptr -> with_matching_exprs nptr n_context
+                      (fun e3 nptr -> builder_f (If0 (e1, e2, e3)) nptr )))
+          ) else
+          (* op1 *)
+          if op = "not" then (
+            with_matching_exprs nptr {n_context with allow_not = false}
+              (fun e1 nptr -> builder_f (Not e1) nptr )
+          ) else
+          if op = "shl1" then (
+            with_matching_exprs nptr context_skip_zero
+              (fun e1 nptr -> builder_f (Shl1 e1) nptr )
+          ) else
+          if op = "shr1" then (
+            with_matching_exprs nptr context_skip_zero
+              (fun e1 nptr -> builder_f (Shr1 e1) nptr )
+          ) else
+          if op = "shr4" then (
+            with_matching_exprs nptr context_skip_zero
+              (fun e1 nptr -> builder_f (Shr4 e1) nptr )
+          ) else
+          if op = "shr16" then (
+            with_matching_exprs nptr context_skip_zero
+              (fun e1 nptr -> builder_f (Shr16 e1) nptr )
+          ) else
+          (* op2 *)
+          if op = "and" then (
+            with_matching_exprs nptr context_skip_zero
+              (fun e1 nptr -> with_matching_exprs nptr context_skip_zero
+                  (fun e2 nptr -> builder_f (And (e1, e2)) nptr ))
+          ) else
+          if op = "or" then (
+            with_matching_exprs nptr context_skip_zero
+              (fun e1 nptr -> with_matching_exprs nptr context_skip_zero
+                  (fun e2 nptr -> builder_f (And (e1, e2)) nptr ))
+          ) else
+          if op = "xor" then (
+            with_matching_exprs nptr context_skip_zero
+              (fun e1 nptr -> with_matching_exprs nptr context_skip_zero
+                  (fun e2 nptr -> builder_f (And (e1, e2)) nptr ))
+          ) else
+          if op = "plus" then (
+            with_matching_exprs nptr context_skip_zero
+              (fun e1 nptr -> with_matching_exprs nptr context_skip_zero
+                  (fun e2 nptr -> builder_f (And (e1, e2)) nptr ))
+          ) else
+          if op = "tfold" then (
+            with_matching_exprs nptr { n_context with allow_fold = false }
+              (fun e1 nptr -> with_matching_exprs nptr { n_context with allow_fold = false; inside_fold = true }
+                  (fun e2 nptr -> builder_f (Fold (e1, E_0, "y1", "y2", e2)) nptr ))
+          ) else
+          if op = "fold" then (
+            with_matching_exprs nptr { n_context with allow_fold = false }
+              (fun e1 nptr -> with_matching_exprs nptr { n_context with allow_fold = false }
+                  (fun e2 nptr -> with_matching_exprs nptr { n_context with allow_fold = false; inside_fold = true }
+                      (fun e3 nptr -> builder_f (Fold (e1, e2, "y1", "y2", e3)) nptr )))
+          ) else failwith ("what is " ^ op);
+        end
+
+        in
+          if ptr >= desc.problem_size then raise Nuff;
+          let r = (( dna.(ptr) mod (n_ops + 5) ) - 5) in 
+          (* Printf.printf "%3d %!" r; *)
+          do_expr r
+
+    in
+        try
+          ignore( with_matching_exprs 0 default_guess_context ( fun e nptr -> if nptr > 2 then raise (Ret_something ("x", e))) );
+          raise Nuff
+        with (Ret_something prob) -> prob
+
+    in
+
+  let random_dna () =
+    Array.init desc.problem_size (fun idx -> Random.int 0x100000) in
+
+  let rec usable_random_dna () =
+    let dna = random_dna () in
+    (try ignore(problem_of_dna dna); dna with Nuff -> usable_random_dna ())
   in
 
-  while true do
-      ttl := 5000;
+  let count_matching_bits a b =
+    let rec loop accu elem =
+      if elem = 0L then accu else
+      if Int64.logand elem 1L = 1L
+        then loop accu (Int64.shift_right_logical elem 1)
+        else loop (accu - 1) (Int64.shift_right_logical elem 1) in
+    (loop 64 (Int64.logxor a b)) in
+
+
+  let calculate_single_score p arg exp =
+    (* 1 in*)
+    if (Program.eval p arg) = exp then 10 else 0 in
+
+    (* let bits = (count_matching_bits (Program.eval p arg) exp) in bits in *)
+    (* let bits = if bits = 64 then 1000 else 0 in bits in *)
+    (* bits - (String.length (Program.to_s p)) in *)
+    (* (count_matching_bits (Program.eval p arg) exp) in *)
+
+  let calculate_score p args exps =
+    if List.for_all2 (fun arg exp -> Program.eval p arg = exp) args exps then (
+      verify_fn p;
+      raise Candidate_update;
+    );
+    (List.fold_right2 (fun arg exp acc -> acc + calculate_single_score p arg exp) args exps 0 )
+    - (String.length (Program.to_s p) / 10)
+  in
+
+  let update_pool_score entry =
+    let input, output = input_g () in
+    (* Helpers.say "ups: %d" (List.length input); *)
+    { entry with pe_score = calculate_score entry.pe_program input output } in
+
+
+  let new_pool_entry () =
+    let dna = usable_random_dna () in
+    let p = problem_of_dna dna in
+    update_pool_score { pe_dna = dna
+    ; pe_program = p
+    ; pe_score = 0
+    } in
+
+  let print_pool pool =
+    Helpers.say "I have a dream. I have a pool:";
+    List.iter (fun pe -> Helpers.say "Score: %d %s" pe.pe_score (Program.to_s pe.pe_program)) pool in
+
+  let take_best n l =
+    let rec take elems all =
+      if elems = 0 then []
+      else (match all with
+      | h :: t -> h :: (take (elems - 1) t)
+      | _ -> []
+      )
+    in
+
+    take n (List.sort (fun a b -> if a.pe_score <> b.pe_score then b.pe_score - a.pe_score else (if Random.bool () then 1 else -1) ) l)
+    in
+
+  let morph_pool pool =
+
+
+    let rec single_morph pe =
+      let d1 = Array.copy pe.pe_dna in
+      let idx1 = Random.int desc.problem_size in
+      d1.(idx1) <- Random.int 0xffffff;
       try
-        (* with_matching_exprs 0 default_guess_context (fun e nptr -> rot() ; Helpers.say "%s" (Program.to_s ("x", e)); if nptr > 1 then verify_fn ("x", e)); *)
-        with_matching_exprs 0 default_guess_context (fun e nptr -> if nptr > 1 then ( rot(); verify_fn ("x", e)));
-      with Abort_this_crap -> ();
-  done
+        let p = problem_of_dna d1 in
+        update_pool_score { pe with pe_dna = d1; pe_program = p; }
+      with Nuff ->
+        single_morph pe in
+
+    List.fold_right (fun elem accu ->
+      (single_morph elem) :: (single_morph elem) :: (single_morph elem) :: elem :: accu
+    ) pool [new_pool_entry (); new_pool_entry (); new_pool_entry (); new_pool_entry (); new_pool_entry (); ]  in
+
+
+  let rec improve pool =
+    (* print_pool pool; *)
+    let p = (
+      try (take_best 10 (morph_pool pool))
+    with Candidate_update -> pool
+    ) in
+
+    rot();
+    (*
+    ignore( Unix.system "clear" );
+    print_pool p; *)
+    (*
+    let f = List.hd p in
+    Printf.printf "%d %s \r%!" f.pe_score (Program.to_s f.pe_program);
+    *)
+
+    improve p
+  in
+
+  improve []
+
 
 let rec process_random_stuff desc verify_fn : unit =
   Helpers.say "Using process_random_stuff solver";
@@ -300,6 +398,8 @@ let do_your_thing desc =
   let inputs = ref []
   and outputs = ref [] in
 
+  let input_getter () = !inputs, !outputs in
+
   let verifier some_guess =
     (* Helpers.say "???? %s" (Program.to_s some_guess); *)
     if List.for_all2 (fun a b -> Program.eval some_guess a = b) !inputs !outputs then (
@@ -314,9 +414,11 @@ let do_your_thing desc =
   inputs := suitable_first_inputs ();
   outputs := Server.get_eval desc.problem_id !inputs;
   try
-    (* process_random_stuff desc verifier; *)
-    smart_iterate_problemspace desc verifier;
+    if false
+    then process_random_stuff desc verifier
+    else smart_iterate_problemspace desc verifier input_getter;
   with (Server.Solved (problem_id, excellent_source)) -> (
+  (* ignore(Unix.system "/usr/bin/aplay /home/e/ding.wav"); *)
     Helpers.say "SOLVED  %s\n" problem_id;
     ()
   )
